0f38e2448c6dac2c399c6657f430137e8d15c014
strenuousnerd8 <zakkiwat@gmail.com>
2023-07-09 23:54:09 +0530

    Optimized

--- 3 Months Preparation Kit (Hackerrank)/Week 1/Time Conversion.py
+++ 3 Months Preparation Kit (Hackerrank)/Week 1/Time Conversion.py
@@ -18,14 +18,12 @@ def timeConversion(s):
     # Write your code here
     meridian = s[-2:]
     time = s[:-2]
-    if time[:2] == str(12):
-        if meridian == "AM":
+    if meridian == "AM":
+        if time[:2] == str(12):
             return "00" + time[2:]
         else:
             return time
-    elif time[:2] != str(12) and meridian == "AM":
-        return time
-    return str(int(time[:2]) + 12) + time[2:]
+    return str(int(time[:2]) + 12) + time[2:] if time[:2] != str(12) else time
 
 
 if __name__ == "__main__":
